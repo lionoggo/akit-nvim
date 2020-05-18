3f37b048e4db001a4f71b12016422790a74ccdd0
lionoggo <lionoggo@gmail.com>
2020-05-18 13:00:59 +0800

    install script optimized

--- install_akit_nvim.sh
+++ install_akit_nvim.sh
@@ -4,11 +4,11 @@ set -e
 
 cd ~/.nvim_runtime
 
-if [[ -d ~/.config/nvim ]]; then
-	echo 'set runtimepath+=~/.nvim_runtime
-	source  ~/.nvim_runtime/nvim.vim
-	' > ~/.config/nvim/init.vim
-	echo "Installed the Ultimate Vim configuration successfuly! Enjoy : -)"
-else
+if [[ ! -d ~/.config/nvim ]]; then
 	echo 'neovim not installed in your system.'
+	exit 1
 fi
+echo 'set runtimepath+=~/.nvim_runtime
+source  ~/.nvim_runtime/nvim.vim
+' > ~/.config/nvim/init.vim
+echo "Installed the Ultimate Vim configuration successfuly! Enjoy : -)"
