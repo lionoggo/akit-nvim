bebb35008a0635280aba094e3d10eab77dcaa80c
lionoggo <lionoggo@gmail.com>
2020-06-13 16:39:45 +0800

    feat: diff coc-settings.json when first install akit-nvim

--- install_akit_nvim.sh
+++ install_akit_nvim.sh
@@ -5,10 +5,24 @@ set -e
 cd ~/.nvim_runtime
 
 if [[ ! -d ~/.config/nvim ]]; then
-	echo 'neovim not installed in your system.'
-	exit 1
+    echo 'neovim not installed in your system.'
+    exit 1
 fi
 echo 'set runtimepath+=~/.nvim_runtime
 source  ~/.nvim_runtime/nvim.vim
 ' > ~/.config/nvim/init.vim
+
+coc_check(){
+    if [[ -f ~/.config/nvim/coc-settings.json ]]; then
+        local coc_diff=$(git diff ~/.nvim_runtime/coc-settings.json ~/.config/nvim/coc-settings.json)
+        if [[ -n "$coc_diff" ]]; then
+            echo -e "\033[31mcoc-settings.json conflict:  \033[0m"
+            echo "$coc_diff"
+        fi
+    else
+        [ -L ~/.config/nvim/coc-settings.json ] && rm -f ~/.config/nvim/coc-settings.json
+        ln -s ~/.nvim_runtime/coc-settings.json ~/.config/nvim/coc-settings.json
+    fi
+}
+coc_check
 echo "Installed the Ultimate Vim configuration successfuly! Enjoy : -)"
