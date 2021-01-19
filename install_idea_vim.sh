794e66b6b3a8aaffe4def296a67ebea0e33a7768
lionoggo <lionoggo@gmail.com>
2021-01-19 17:10:17 +0800

    feat(idea): use soft-linker when install vim

--- install_idea_vim.sh
+++ install_idea_vim.sh
@@ -14,8 +14,14 @@ rename_if_file_exists(){
 
 rename_if_file_exists
 
-echo 'set runtimepath+=~/.nvim_runtime
-source  ~/.nvim_runtime/idea.vim
-' > ~/.ideavimrc
+# rm -f "$HOME/.ideavimrc" 2>/dev/null
+# ln -s "$PWD/config/idea/basic.vim" "$HOME/.ideavimrc"
+
+rm -f "$HOME/.ideavimrc" 2>/dev/null
+ln -s ~/.nvim_runtime/config/idea/basic.vim "$HOME/.ideavimrc"
+
+# echo 'set runtimepath+=~/.nvim_runtime
+# source  ~/.nvim_runtime/config/idea/basic.vim
+# ' > ~/.ideavimrc
 
 echo "Installed the basic Vim configuration for ideavim successfuly! Enjoy : -)"
