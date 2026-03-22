e8c041563f045bd88d71dfc73f8d4c7a77068d8b
lionoggo <lionoggo@gmail.com>
2026-03-22 22:18:46 +0800

    fix(translate): add normal mode support to prevent key fallthrough to till motion

--- lua/plugins/translate.lua
+++ lua/plugins/translate.lua
@@ -2,8 +2,8 @@ return {
   {
     "uga-rosa/translate.nvim",
     keys = {
-      { "<leader>ts", "<cmd>Translate zh-CN<cr>", mode = "v", desc = "Translate to Chinese" },
-      { "<leader>tr", "<cmd>Translate zh-CN -output=replace<cr>", mode = "v", desc = "Translate & replace" },
+      { "<leader>ts", "<cmd>Translate zh-CN<cr>", mode = { "n", "v" }, desc = "Translate to Chinese" },
+      { "<leader>tr", "<cmd>Translate zh-CN -output=replace<cr>", mode = { "n", "v" }, desc = "Translate & replace" },
     },
     opts = {
       default = {
