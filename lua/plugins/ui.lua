78d8e2e6f951dec67fe90bf81161835517bcfddf
lionoggo <lionoggo@gmail.com>
2026-04-04 12:46:26 +0800

    fix(explorer): move reveal to <leader>E to eliminate prefix delay on <leader>e

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -185,8 +185,8 @@ return {
       },
     },
     keys = {
-      { "<leader>e", function() Snacks.explorer() end, desc = "Toggle explorer", nowait = true },
-      { "<leader>en", function() Snacks.explorer.reveal() end, desc = "Reveal in explorer" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "Toggle explorer" },
+      { "<leader>E", function() Snacks.explorer.reveal() end, desc = "Reveal in explorer" },
       { "<leader>ns", function() Snacks.scratch() end, desc = "Scratch (persistent)" },
       { "<leader>nN", function() vim.cmd("enew") vim.bo.buftype = "nofile" vim.bo.bufhidden = "wipe" end, desc = "New temp buffer" },
       { "<leader>gg", function() Snacks.terminal("lazygit") end, desc = "Lazygit" },
