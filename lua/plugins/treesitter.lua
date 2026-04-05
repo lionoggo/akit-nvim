bed7699d0058366bdece4b55013ab64d242c9faf
lionoggo <lionoggo@gmail.com>
2026-04-05 17:51:30 +0800

    fix(treesitter): Update config to show highlight

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    lazy = false,
     build = ":TSUpdate",
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     config = function()
